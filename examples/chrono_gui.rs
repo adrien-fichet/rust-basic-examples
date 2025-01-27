a2e87686ff7ad47a655c84f4301ea22384ae7286
Adrien FICHET <adrien.fichet@gmail.com>
2025-01-27 23:02:33 +0100

    Update chrono_gui example

--- examples/chrono_gui.rs
+++ examples/chrono_gui.rs
@@ -1,37 +1,46 @@
-use iced::widget::{button, column, container, text};
+use iced::widget::{button, column, row, text};
 use iced::Element;
+use std::time::Duration;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    using_iced()?;
-    //using_egui()?;
+    iced_app()?;
     Ok(())
 }
 
-fn using_egui() {
-    todo!();
-}
-
-fn using_iced() -> iced::Result {
-    iced::run("Chrono", iced_update, iced_view)?;
+fn iced_app() -> iced::Result {
+    // todo: configure basic window settings
+    iced::run("Chrono", chrono_update, chrono_view)?;
     Ok(())
 }
 
 #[derive(Debug, Clone)]
-enum IcedMessage {
-    Increment,
+enum ChronoMessage {
+    StartStop,
+    Pause,
+    Reset,
 }
 
-fn iced_update(counter: &mut u64, message: IcedMessage) {
+fn chrono_update(chrono: &mut Duration, message: ChronoMessage) {
     match message {
-        IcedMessage::Increment => *counter += 1,
+        ChronoMessage::StartStop => {
+            // todo: start and stop chrono in a thread instead
+            *chrono += Duration::from_secs(1);
+        }
+        ChronoMessage::Pause => *chrono = Duration::from_secs(1),
+        ChronoMessage::Reset => *chrono = Duration::from_secs(0),
     }
 }
 
-fn iced_view(counter: &u64) -> Element<IcedMessage> {
+fn chrono_view(chrono: &Duration) -> Element<ChronoMessage> {
     column![
-        text(counter).size(20),
-        button("Increment").on_press(IcedMessage::Increment),
+        text(chrono.as_secs()).size(50),
+        row![
+            button("Start/Stop").on_press(ChronoMessage::StartStop),
+            button("Pause").on_press(ChronoMessage::Pause),
+            button("Reset").on_press(ChronoMessage::Reset),
+        ]
+        .spacing(20),
     ]
-    .spacing(10)
+    .spacing(20)
     .into()
 }
