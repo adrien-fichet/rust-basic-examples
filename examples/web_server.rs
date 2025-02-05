df83b59c119cd728a3a584a3bd664dc7d60c98fb
Adrien FICHET <adrien.fichet@gmail.com>
2025-02-05 22:06:16 +0100

    Update the web_server example

--- examples/web_server.rs
+++ examples/web_server.rs
@@ -1,3 +1,42 @@
-fn main() {
-    todo!();
+/*
+curl -v http://127.0.0.1:3000
+curl -v http://127.0.0.1:3000/hello-json | jq
+curl -v http://127.0.0.1:3000/now | jq
+
+todo:
+- extract path and body params
+- error handling
+- middleware
+- shared state
+*/
+
+use axum::{response::Json, routing::get, Router};
+use serde_json::{json, Value};
+use std::error::Error;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+async fn hello_world() -> &'static str {
+    "Hello, World!"
+}
+
+async fn hello_json() -> Json<Value> {
+    Json(json!({"message": "Hello, World!", "data": 42}))
+}
+
+async fn now() -> Json<Value> {
+    Json(json!({"now": SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()}))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let app = Router::new()
+        .route("/", get(hello_world))
+        .route("/hello-json", get(hello_json))
+        .route("/now", get(now));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
+    println!("Serving on http://127.0.0.1:3000 ...");
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
